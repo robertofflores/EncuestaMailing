69bc53005df1f83121c48ed94ae0e9e49e966d55
Roberto Flores <robertofflores@hotmail.com>
2021-05-04 12:37:56 -0500

    Se agergó  funcionalidad para enviar BONOS al proveedor de mailing

---
+++ EncuestaMailing/Data/Bono.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncuestaMailing.Data
+{
+    public class Bono
+    {
+        public int codigo { get; set; }
+        public double valor { get; set; }
+		public String beneficiario { get; set; }
+		public String fecha { get; set; }
+		public String tipo { get; set; }
+		public String cedula { get; set; }
+		public String email { get; set; }
+		public String concepto { get; set; }
+
+		public String observacion { get; set; }
+		
+	}
+}
